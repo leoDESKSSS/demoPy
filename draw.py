3f3018f34a59afbe6bee57a055c4ed80dbac6666
leo <leodesksss@nit.zju.edu.cn>
2020-10-16 00:17:34 +0800

    main

---
+++ draw.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt   # 导入模块 matplotlib.pyplot，并简写成 plt 
+import numpy as np                # 导入模块 numpy，并简写成 np
+
+x = np.linspace(-50, 50, 5000)
+y = np.sin(x)
+
+y1 = 4*x**2 + 5*x + 6    #二次函数
+y2 = np.exp(x)     #指数
+
+
+
+plt.plot(x, y)
+
+plt.figure()
+plt.plot(x, y1)
+
+plt.figure()
+plt.plot(x, y2)
+
+
+
+
+
+plt.show()
